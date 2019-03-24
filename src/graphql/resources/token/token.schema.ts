2ecaee933d5b207300d092c5ca28765bba000b30
manoel freitas <manoel.josefneto@gmail.com>
2019-03-23 22:36:59 -0300

    primeiro commit

---
+++ src/graphql/resources/token/token.schema.ts
@@ -0,0 +1,17 @@
+const tokenTypes = `
+
+    type Token {
+        token: String!
+    }
+
+  
+  
+`
+const tokenMutations = `
+    createToken(email: String!, password: String!): Token
+`
+
+export {
+    tokenTypes,
+    tokenMutations
+}
